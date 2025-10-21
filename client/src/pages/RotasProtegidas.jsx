f1b3909c93df8516679986e98150800a0611458f
JAVA - ESTUDOS <vfernandesfn@gmail.com>
2025-10-21 10:57:56 -0300

    alterações

---
+++ client/src/pages/RotasProtegidas.jsx
@@ -0,0 +1,40 @@
+// Importação do navigate pra transitar entre páginas
+// Importando o outlet, do REA
+import { Outlet, Navigate } from "react-router-dom"
+
+// Importação dos componentes do bootstrap
+import Container from "react-bootstrap/Container"
+
+// Importar as informações do contexto autenticação de usuário
+import { AuthContext } from '../contexts/UserContext.jsx'
+import { useContext } from "react";
+
+const RotasProtegidas = () => {
+  // Pega a variável de usuario nome pra saber se tem algum logado
+  const { usuarioNome } = useContext(AuthContext)
+  
+  if ( usuarioNome === "Visitante"){
+    return <Navigate to="/login"/>
+  }
+
+  return (
+    <div className="App d-flex">
+
+     {/* Barra de navegação fixa na lateral */}
+     <div className="position-fixed top-0 start-0 min-vh-100 bg-danger">
+        <h1>Usuário logado:</h1>
+        <h1>{usuarioNome}</h1>
+     </div>
+
+     {/* Conteúdo principal, dependendo de qual rota está */}
+     <div className="d-flex flex-column min-vh-100 flex-grow-1 p-2 justify-content-center"
+          style={{marginLeft:"350px"}}>
+        <Container fluid>
+          <h1>Conteúdo principal</h1>
+        </Container>
+      </div> 
+    </div>
+  )
+}
+
+export default RotasProtegidas
